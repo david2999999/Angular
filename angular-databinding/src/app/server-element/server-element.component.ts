84eaebd15f5c81d796a5fae265f21ed714266b64
David Jiang <djiang86@binghamton.edu>
2021-02-26 01:45:49 -0500

    recipe item component, passing recipe

--- angular-databinding/src/app/server-element/server-element.component.ts
+++ angular-databinding/src/app/server-element/server-element.component.ts
@@ -1,14 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: { type: string, name: string, content: string };
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+  }
 
-  ngOnInit(): void { }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
 }
